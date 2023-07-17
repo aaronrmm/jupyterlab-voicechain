b108d90f239b0c33539feed2cec9b1199872da51
Aaron <aaronrmm@gmail.com>
2023-07-16 22:34:56 -0400

    refactor activate function

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+import { ICommandPalette } from '@jupyterlab/apputils';
 /**
  * Initialization data for the jupyterlab-voicechain extension.
  */
+
+const activate_func = (app: JupyterFrontEnd, palette: ICommandPalette) =>{
+  console.log('JupyterLab extension jupyterlab-voicechain.activate_func is activated!');
+}
+
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-voicechain:plugin',
   description: 'A Jupyterlab extension that provides voice commands for AI code completion with LangChain.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab-voicechain is activated!');
-  }
+  activate: activate_func
 };
 
 export default plugin;
